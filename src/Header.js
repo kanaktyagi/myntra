6c07ddb5151f33bbc7e42396f3e3c5c7affbc716
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-16 21:04:36 +0530

    Completed the header Component

--- src/Header.js
+++ src/Header.js
@@ -11,6 +11,21 @@ function Header() {
           src={image}
           alt="image not available"
           />
+          <div className="header__option"> 
+          <span className="header__optionLineOne"><h4>MEN</h4></span>
+          <span className="header__optionLineOne"><h4>WOMEN</h4></span>
+          <span className="header__optionLineOne"><h4>KIDS</h4></span>
+          <span className="header__optionLineOne"><h4>HOME & LIVING</h4></span>
+          <span className="header__optionLineOne"><h4>OFFER</h4></span>
+          <div className="desktop__query">
+          <input placeholder="Search for products, brands and more" className="desktop__searchBar" value=""></input>
+          <div className="header__option"> 
+          <span className="header__optionLineOne"><h4>PROFILE</h4></span>
+          <span className="header__optionLineOne"><h4>WISHLIST</h4></span>
+          <span className="header__optionLineOne"><h4>BAGS</h4></span>
+          </div>
+          </div>
+          </div>
         </nav>
       </div>
     )
