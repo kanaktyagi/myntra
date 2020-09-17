ee176fcd5abf93e57f38fc48c9f1448c27eef11f
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-17 22:08:24 +0530

    Added the modular main.js file and the delay of 2 sec to get json.data file

--- src/Main.js
+++ src/Main.js
@@ -1,31 +1,24 @@
 import React,{useEffect,useState} from 'react'
-import './Main.css'
 import data from './data1.json'
+import Products from './Products'
 
 function Main() {
     const [products, setproduct] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
+        console.log(loading)
+       setTimeout(()=>{
         setproduct(data.data.products)
+       }, 2000) 
+        setLoading(false);
         console.log(products)
       },[products]);
-
     
     return (
-        <div className="product">
-         {products.map((product) => (
-             <div className="product__info">
-            <img className="product__image"
-            src={product.categoryImages.map(url => url)}
-            alt="cant display image"
-          />
-       <div className="product__name">  {product.productData.categoryName}</div>
-       
-          <span style={{paddingLeft: "15px",paddingBottom: "10px"}}>₹</span> <strong >{parseInt(product.productData.specialPrice).toLocaleString()}</strong><span style={{textDecoration: "line-through", paddingLeft: "10px",  color: "rgb(124, 121, 121)", fontSize: "12px"}}>{parseInt(product.productData.price).toLocaleString()} </span>
-          <div><small style={{ paddingLeft: "15px",color: "rgb(124, 121, 121)", fontSize: "12px"}}>{product.productData.discountPercentage} off on making charges</small></div>
-          </div>
-         )
-         )}
+        <div>
+        < Products products={products} loading={loading} />
         </div>
     )
 }
