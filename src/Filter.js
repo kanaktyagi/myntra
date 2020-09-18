996a0bda9016bf88e1707303bebd7159ce855d15
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-18 16:07:20 +0530

    Added the code for checkbox selection

--- src/Filter.js
+++ src/Filter.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Filter.css'
 
-function Filter() {
+function Filter({ parentCallback }) {
+    const [value,setValue] = useState();
+    const [checkedValue, setCheckedValue] = useState([]);
+
+    const handleChanged =(event) => {
+        const target = event.target;
+        const name = target.name;
+        setCheckedValue([name])
+        // parentCallback([name])
+        if(target.checked){
+     setCheckedValue([...checkedValue,name])
+    //  parentCallback([...checkedValue,name]);
+        }else { 
+           setCheckedValue(checkedValue.filter(value => value!== name ))
+        //    parentCallback([checkedValue])
+        } 
+        parentCallback([checkedValue])
+    }
     return (
         <div>
-            
+            {console.log(checkedValue)}
             <div className="filter">
             <div className="vertical__filters__filters">
             <h4 className="Filter__heading">FILTERS </h4>
@@ -13,25 +30,37 @@ function Filter() {
             <ul>
             <li>
             <label>
-            <input type="checkbox"></input>
+            <input 
+            type="checkbox" 
+            onChange={handleChanged}
+            checked={value}
+             name="10,000"></input>
             10,0000
             </label>
             </li>
             <li>
             <label>
-            <input type="checkbox"></input>
+            <input type="checkbox"
+            onChange={handleChanged}
+            checked={value}
+             name="20,000"
+            ></input>
             20,0000
             </label>
             </li>
             <li>
             <label>
-            <input type="checkbox"></input>
+            <input type="checkbox"
+            onChange={handleChanged}
+            checked={value}
+             name="30,000"
+            ></input>
             30,0000
             </label>
             </li>
             </ul>
             </div>
-            
+           { parentCallback([checkedValue])}
 
             </div>
            
