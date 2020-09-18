750c8b9751649b807dabe9a6d9030a7f731a9cd9
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-09-18 16:11:57 +0530

    Added the filter component to the Main component

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,12 @@
 import React,{useEffect,useState} from 'react'
 import data from './data1.json'
 import Products from './Products'
+import Filter from './Filter'
 
 function Main() {
     const [products, setproduct] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [filter, setFilter] = useState([]);
 
     useEffect(() => {
         setLoading(true);
@@ -15,9 +17,19 @@ function Main() {
         setLoading(false);
         console.log(products)
       },[products]);
-    
+
+    const  callback = ([count]) => {
+        // do something with value in parent component, like save to state
+        // setFilter([count])
+    }
+
     return (
-        <div>
+        <div style={{ display:"flex"}}>
+        {console.log("filter", filter)}
+        {console.log("hiiiii")}
+    
+       <Filter parentCallback={callback}/> 
+       
         < Products products={products} loading={loading} />
         </div>
     )
